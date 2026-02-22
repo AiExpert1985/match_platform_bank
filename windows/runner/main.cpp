08791bf3c507298f9a4f7b9401204ad13e7fe183
Hamandi <aiexpert2025@gmail.com>
2026-02-22 13:31:47 +0300

    fix the issue that flutter windows exe not working under arabic named parent folder

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,9 +1,23 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <string>
 
 #include "flutter_window.h"
 #include "utils.h"
+
+std::wstring GetExecutableDirectory() {
+  wchar_t buffer[MAX_PATH];
+  if (GetModuleFileNameW(nullptr, buffer, MAX_PATH) == 0) {
+    return std::wstring();
+  }
+  std::wstring path(buffer);
+  size_t last_slash = path.find_last_of(L"\\/");
+  if (last_slash != std::wstring::npos) {
+    return path.substr(0, last_slash);
+  }
+  return std::wstring();
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
@@ -17,7 +31,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-  flutter::DartProject project(L"data");
+  std::wstring base_directory = GetExecutableDirectory();
+  std::wstring data_directory = base_directory.empty() ? L"data" : base_directory + L"\\data";
+  flutter::DartProject project(data_directory);
 
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
