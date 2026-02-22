74bd3d85cc12ed07429c49a19d996c4b4295cc68
Hamandi <aiexpert2025@gmail.com>
2026-02-22 12:50:30 +0300

    fix windows build issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:match_platform_bank/features/transactions/presentation/screens/main_screen.dart';
+import 'dart:io';
+import 'dart:async';
 
 void main() {
-  runApp(const ProviderScope(child: App()));
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      FlutterError.onError = (FlutterErrorDetails details) {
+        logError(details.exceptionAsString(), details.stack);
+      };
+      runApp(const ProviderScope(child: App()));
+    },
+    (error, stackTrace) {
+      logError(error.toString(), stackTrace);
+    },
+  );
+}
+
+void logError(String error, StackTrace? stack) {
+  try {
+    File('crash_log.txt').writeAsStringSync(
+      'Error: $error\nStack: $stack\n',
+      mode: FileMode.append,
+    );
+  } catch (_) {}
 }
 
 class App extends StatelessWidget {
@@ -17,10 +39,8 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
       home: const MainScreen(),
-      builder: (context, child) => Directionality(
-        textDirection: TextDirection.rtl,
-        child: child!,
-      ),
+      builder: (context, child) =>
+          Directionality(textDirection: TextDirection.rtl, child: child!),
     );
   }
 }
